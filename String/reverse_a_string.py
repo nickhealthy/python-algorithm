d37fbb6cbea84472e97050f6b02fcbab6200abee
nick <alskadmlcraz1@gmail.com>
2021-02-01 10:43:19 +0900

    UPDATE: command

--- String/reverse_a_string.py
+++ String/reverse_a_string.py
@@ -1,6 +1,7 @@
 # 1번 문자열 거꾸로 뒤집기
 # Reversing a string using slicing
 
+import collections
 my_string = "ABCDE"
 reversed_string = my_string[::-1]
 
@@ -20,3 +21,22 @@ else:
 
 # Output
 # palindrome
+
+
+# 3번 팰린드룸
+def isPalindrome(S: str) -> bool:
+    strs: Deque = collections.deque()
+
+    for char in S:
+        # 문자와 숫자 판별
+        if char.isalnum():
+            strs.append(char.lower())
+    # 팰린드롬 여부 판별
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+
+    return True
+
+
+print(isPalindrome("race a Car"))
