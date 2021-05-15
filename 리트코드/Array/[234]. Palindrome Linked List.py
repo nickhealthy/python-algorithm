492f7e1fa03ca643b57142fd5fa1bb256f54771e
nick <jobinformation@naver.com>
2021-05-15 16:30:54 +0900

    리트코드 - [234]. Palindrome Linked List

---
+++ 리트코드/Array/[234]. Palindrome Linked List.py
@@ -0,0 +1,34 @@
+import collections
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head):
+        q = collections.deque()
+
+        if not head:
+            return True
+
+        node = head
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+        
+        return True
+
+
+head = [1,2]
+# Output: true
+
+test = Solution()
+print(test.isPalindrome(head))
+
+
