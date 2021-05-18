22bc58dd1d0883a2fb0e242e0a6b5e0c81e1c918
nick <jobinformation@naver.com>
2021-05-18 13:51:25 +0900

    백준 - [1260]-DFS와BFS

--- 백준/DFS,BFS/[1260]-DFS와BFS.py
+++ 백준/DFS,BFS/[1260]-DFS와BFS.py
@@ -1,25 +1,14 @@
-import sys
-import collections
-
-read = sys.stdin.readline
-n, m, v = map(int, read().split())
-graph = [[0] * (n + 1) for _ in range(n + 1)]
-visited = [False] * (n + 1)
-
-for _ in range(m):
-    line = list(map(int, read().split()))
-    graph[line[0]][line[1]] = graph[line[1]][line[0]] = 1
+from collections import deque
 
 def dfs(v):
-    visited[v] = True # 방문 처리
+    visited[v] = True
     print(v, end=' ')
     for i in range(1, n + 1):
         if not visited[i] and graph[v][i] == 1:
             dfs(i)
 
-
 def bfs(v):
-    queue = collections.deque([v])
+    queue = deque([v])
     visited[v] = False
     while queue:
         v = queue.popleft()
@@ -28,6 +17,15 @@ def bfs(v):
             if visited[i] and graph[v][i] == 1:
                 queue.append(i)
                 visited[i] = False
+
+
+n, m, v = map(int, input().split()) # 정점, 간선, 시작점
+visited = [False for _ in range(n + 1)] # 방문 설정
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+# 매트릭 설정
+for _ in range(m):
+    line = list(map(int, input().split()))
+    graph[line[0]][line[1]] = graph[line[1]][line[0]] = 1
 
 dfs(v)
 print()
