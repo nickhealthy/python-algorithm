84cc1d5d89b019d1bc2c17733e712dceb8396127
nick <jobinformation@naver.com>
2021-05-22 15:36:28 +0900

    백준 - [9372]-상근이의 여행

---
+++ 백준/그래프, 트리/[9372]-상근이의 여행.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+case = int(input())
+for _ in range(case):
+    n, m = map(int, input().split())
+    for _ in range(m):
+        line = input().split()
+    
+    print(n - 1)
