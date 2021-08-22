56b9f59935fbef5223cf9e64b036a573658dea9c
nick <jobinformation@naver.com>
2021-08-23 00:03:11 +0900

    [카카오문제]_문자열압축.py

---
+++ 프로그래머스/문자열처리/[카카오문제]_문자열압축.py
@@ -0,0 +1,38 @@
+def solution(s):
+    answer = len(s)
+    
+    # i는 반복되는 문자열의 길이를 의미
+    for i in range(1, int(len(s)/2) + 1):
+        pos = 0
+        length = len(s)
+
+        while pos + i <= len(s):
+            unit = s[pos : pos + i]
+            pos += i
+
+            cnt = 0
+            while pos + i <= len(s):
+                if unit == s[pos : pos + i]:
+                    cnt += 1
+                    pos += i
+                else:
+                    break
+            
+            if cnt > 0:
+                length -= i * cnt
+
+                if cnt < 9:
+                    length += 1
+                elif cnt < 99:
+                    length += 2
+                elif cnt < 999:
+                    length += 3
+                else:
+                    length += 4
+                
+            answer = min(answer, length)
+
+    return answer
+
+s = "xababcdcdababcdcd"
+print(solution(s))
