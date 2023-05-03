b80492247695158f53949a377a30afeae9b3d2a4
nickhealthy <alskadmlcraz1@gmail.com>
2023-05-03 19:05:38 +0900

    programmers - LV1_바탕화면정리

---
+++ 프로그래머스/구현/[LV1]_바탕화면정리.py
@@ -0,0 +1,12 @@
+def solution(wallpaper):
+    a, b = [], []
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[i])):
+            if wallpaper[i][j] == '#':
+                a.append(i)
+                b.append(j)
+    
+    if not a and not b:
+        return -1
+
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
