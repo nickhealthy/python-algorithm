bc9a06872b9800d4ee69dacc8e728becdb0f7aca
nick <alskadmlcraz1@gmail.com>
2023-05-07 19:04:23 +0900

    [unrated] Title: 배열 만들기 2, Time: 29.63 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,17 +1,26 @@
+# def solution(l, r):
+#     answer = []
+    
+#     for i in range(l, r + 1):
+#         if i % 5 == 0:
+#             tmp = ''
+#             for j in str(i):
+#                 if j == '5' or j == '0':
+#                     tmp += j
+#                 else:
+#                     break
+#             else:
+#                 answer.append(int(tmp))
+    
+#     return answer if answer else [-1]
+
+# print(solution(5, 555))
+
 def solution(l, r):
     answer = []
     
     for i in range(l, r + 1):
-        if i % 5 == 0:
-            tmp = ''
-            for j in str(i):
-                if j == '5' or j == '0':
-                    tmp += j
-                else:
-                    break
-            else:
-                answer.append(int(tmp))
-    
-    return answer if answer else [-1]
+        if not set(str(i)) - set(['0', '5']):
+            answer.append(i)
 
-print(solution(5, 555))
+    return answer if answer else [-1]
