7805f52c3468fc39f4c77978e2119fe955bb1297
nick <alskadmlcraz1@gmail.com>
2023-05-22 18:05:41 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.55 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,15 +1,32 @@
+# def solution(s):
+#     answer = []
+
+#     tmp = []
+#     for idx, val in enumerate(s):
+#         if val not in tmp:
+#             tmp.append(val)
+#             answer.append(-1)
+
+#         elif val in tmp:
+#             element = abs(idx - len(tmp) - tmp[::-1].index(val) - 1)
+#             tmp.append(val)
+#             answer.append(element)
+
+#     return answer
+
 def solution(s):
     answer = []
 
-    tmp = []
+    dic = {}
     for idx, val in enumerate(s):
-        if val not in tmp:
-            tmp.append(val)
+        if val not in dic:
             answer.append(-1)
+        else:
+            answer.append(idx - dic[val])
 
-        elif val in tmp:
-            element = abs(idx - len(tmp) - tmp[::-1].index(val) - 1)
-            tmp.append(val)
-            answer.append(element)
+        dic[val] = idx
 
     return answer
+
+
+print(solution("banana"))
