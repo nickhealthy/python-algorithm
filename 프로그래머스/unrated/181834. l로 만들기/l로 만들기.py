64db77788342a67fd8f5d92edb90ead1fe4fcaf6
nick <alskadmlcraz1@gmail.com>
2023-05-24 19:38:30 +0900

    [unrated] Title: l로 만들기, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,9 +1,12 @@
+# def solution(myString):
+#     answer = ''
+#     for i in myString:
+#         print(i)
+#         if i < "l":
+#             answer += 'l'
+#             continue
+#         answer += i
+#     return answer
+
 def solution(myString):
-    answer = ''
-    for i in myString:
-        print(i)
-        if i < "l":
-            answer += 'l'
-            continue
-        answer += i
-    return answer
+    return myString.translate(str.maketrans('abcdefghijk', 'lllllllllll'))
