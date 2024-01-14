c771280898e33e3f7a4a364e0b738e97222dae38
nick <alskadmlcraz1@gmail.com>
2024-01-14 11:08:54 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -5,30 +5,33 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        int answer = 0;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String params = br.readLine();
 
-        String[] arr = params.split("\\-");
-        String[] sumArr = arr[0].split("\\+");
-        for (int i = 0; i < sumArr.length; i++) {
-            answer += Integer.valueOf(sumArr[i]);
-        }
-
-        // 첫 번째로 나오는 마이너스를 기준으로 왼쪽은 더해주고, 오른쪽은 모두 빼주면 됨
-        // 첫 번째는 무조건 양수
-        for (int i = 1; i < arr.length; i++) {
-            String[] localArr = arr[i].split("\\+");
-            for (int j = 0; j < localArr.length; j++) {
-                answer -= Integer.valueOf(localArr[j]);
-            }
-        }
-
+        int answer = calculate(params);
         System.out.println(answer);
-
-
     }
 
+    private static int calculate(String params) {
+        String[] arr = params.split("\\-");
+
+        // 첫 번째로 나오는 마이너스를 기준으로 왼쪽은 더해주고, 오른쪽은 모두 빼주면 됨
+        int answer = sum(arr[0]);
+
+        // 첫 번째는 무조건 양수
+        for (int i = 1; i < arr.length; i++) {
+            answer -= sum(arr[i]);
+        }
+
+        return answer;
+    }
+
+    private static int sum(String s) {
+        String[] sumArr = s.split("\\+");
+        int result = 0;
+        for (String num : sumArr) {
+            result += Integer.parseInt(num);
+        }
+        return result;
+    }
 }
