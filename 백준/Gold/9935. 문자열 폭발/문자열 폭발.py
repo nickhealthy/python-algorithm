d3913a6032e622432c51887e6e0b607bc48a8061
nick <alskadmlcraz1@gmail.com>
2024-01-17 14:42:40 +0900

    [Gold IV] Title: 문자열 폭발, Time: 920 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,41 +1,27 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-string = list(input().rstrip())
-bomb = list(input().rstrip())
+orginal_strings = input().strip()
+bomb_string = input().strip()
 
-stack = deque()
-bomb_length = len(bomb)
-string_length = len(string)
+BOMB_STRING_LENGTH = len(bomb_string)
 
+stack = []
+for char in orginal_strings:
+    stack.append(char)
 
-for i in range(string_length):
-    stack.append(string[i])
-    # 폭탄 문자열 확인
-    while True:
-        flag = False
-
-        if string_length < bomb_length:  # bomb_length가 더 길다면
+    isBombStringCount = 0
+    for j in range(BOMB_STRING_LENGTH):
+        isBombStringCount += 1
+        if stack[len(stack) - 1 - j] != bomb_string[BOMB_STRING_LENGTH - 1 - j]:
             break
 
-        for i in range(bomb_length):
-            if stack[len(stack) - 1 - i] != bomb[bomb_length - 1 - i]:
-                flag = True
-                break
-
-        if flag:
-            break
-
-        # 위의 폭탄 문자열을 확인해봤을 때 여기까지 넘어오면 폭탄 문자열이 포함되어 있다는 뜻
-        # 폭탄 문자열 수만큼 스택 팝
-        for _ in range(bomb_length):
+    else:
+        # 스택에 폭발 문자열이 들어있다면
+        for _ in range(BOMB_STRING_LENGTH):
             stack.pop()
 
-        break
-
-
-if not stack:
-    print('FRULA')
-else:
+if stack:
     print("".join(stack))
+else:
+    print("FRULA")
