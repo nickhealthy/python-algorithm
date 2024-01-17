8928d6edfbea77ed2d031f1d4f5cf34cea74ba81
nick <alskadmlcraz1@gmail.com>
2024-01-17 18:25:53 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -9,28 +10,39 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         final int N = Integer.parseInt(br.readLine());
-        System.out.println(solution(N));
+        int start = N - String.valueOf(N).length() * 9;
 
+        /* 스트림 두 번째 풀이 */
+        int answer = IntStream.range(start, N)
+                .filter(value ->
+                        String.valueOf(value).chars().map(Character::getNumericValue).sum() + value
+                                == N)
+                .findFirst()
+                .orElse(0);
 
-        /*
-         * 1.스트링
-         * 스트링 길이는 가능
-         * 스트링 toCharArray 가능
-         * */
+        /* 스트림 첫 번째 풀이 */
+//        int answer = 0;
+//        for (int i = start; i < N; i++) {
+//
+//            int sum = String.valueOf(i)
+//                    .chars()
+//                    .map(Character::getNumericValue)
+//                    .sum();
+//
+//            if (sum + i == N) {
+//                answer = i;
+//                break;
+//            }
+//        }
 
-        /*
-         * 2. 스트링 배열
-         * 스트링 길이 가능
-         * 스트링 배열 접근 가능
-         * */
+        /* 정통적인 방식 첫 번째 풀이 */
+//        answer = solution(N);
 
-        /*
-         * 3. 스트림
-         *  스트링 배열로 만들어서 형 변환 및 합계까지 구한다.
-         * */
+        System.out.println(answer);
 
     }
 
+    /* 정통적인 방식 첫 번째 풀이 */
     private static int solution(int N) {
 
         for (int i = 1; i < N; i++) {
