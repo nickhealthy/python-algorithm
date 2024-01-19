dacfdeb04121d6338ed4a11cd9ac59f8ae8b5d4a
nick <alskadmlcraz1@gmail.com>
2024-01-19 19:52:52 +0900

    [Gold IV] Title: 가르침, Time: 332 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,69 +1,74 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
- 
-    static int n, k;
-    static int max = Integer.MIN_VALUE;
-    static boolean[] visited;
-    static String[] word;
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        n = scan.nextInt();
-        k = scan.nextInt();
-        
-        scan.nextLine();
-        word = new String[n];
-        for(int i = 0; i < n; i++) {
-            String str = scan.nextLine();
-            str = str.replace("anta", "");
-            str = str.replace("tica", "");
-            word[i] = str;
-        }
-        
-        if(k < 5) { //a c i n t의 개수가 5개이므로
+
+    static int N, K, answer;
+    static boolean[] visited = new boolean[26];
+    static String[] words;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.valueOf(input[0]);
+        K = Integer.valueOf(input[1]);
+
+        if (K < 5) {
             System.out.println("0");
             return;
-        } else if(k == 26) { //모든 알파벳을 다 읽을 수 있다.
-            System.out.println(n);
+        } else if (K == 26) {
+            System.out.println(N);
             return;
         }
-        
-        visited = new boolean[26]; //각 알파벳을 배웠는지 체크
-        visited['a' - 'a'] = true;
-        visited['c' - 'a'] = true;
-        visited['i' - 'a'] = true;
-        visited['n' - 'a'] = true;
-        visited['t' - 'a'] = true;
-        
-        backtracking(0, 0);
-        System.out.println(max);
+
+        words = new String[N];
+        for (int i = 0; i < N; i++) {
+            String parseString = br.readLine();
+            words[i] = parseString.substring(4, parseString.length() - 4);
+        }
+
+        // visted base_char 초기화
+        for (char c : "acint".toCharArray()) {
+            visited[c - 'a'] = true;
+        }
+
+        backTracking(0, 0);
+        System.out.println(answer);
     }
-    
-    public static void backtracking(int alpha, int len) {
-        if(len == k - 5) {
+
+    private static void backTracking(int alpha, int len) {
+
+        if (len == K - 5) {
             int count = 0;
-            for(int i = 0; i < n; i++) { //뽑은 알파벳으로 몇개의 단어를 읽을 수 있는지 카운트.
+            for (int i = 0; i < words.length; i++) { // 뽑은 알파벳으로 몇 개의 단어를 읽을 수 있는지 카운트
                 boolean read = true;
-                for(int j = 0; j < word[i].length(); j++) {
-                    if(!visited[word[i].charAt(j) - 'a']) {
+
+                for (int j = 0; j < words[i].length(); j++) {
+                    if (!visited[words[i].charAt(j) - 'a']) {
                         read = false;
                         break;
                     }
                 }
-                if(read) count++;
+
+                if (read) {
+                    count++;
+                }
             }
-            max = Math.max(max, count);
+
+            answer = Math.max(answer, count);
             return;
         }
-        
-        for(int i = alpha; i < 26; i++) {
-            if(visited[i] == false) {
+
+        for (int i = alpha; i < 26; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
-                backtracking(i, len + 1);
+                backTracking(i, len + 1);
                 visited[i] = false;
             }
         }
+
     }
+
 }
