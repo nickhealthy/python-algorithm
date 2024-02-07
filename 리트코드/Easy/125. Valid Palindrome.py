148f525cad9f09ba7f4cc8a4423b70c50247c4ce
nickhealthy <alskadmlcraz1@gmail.com>
2024-02-07 14:54:08 +0900

    리트코드_EASY - 125. Valid Palindrome

--- 리트코드/Easy/125. Valid Palindrome.py
+++ 리트코드/Easy/125. Valid Palindrome.py
@@ -1,8 +1,26 @@
-s = "A man, a plan, a canal: Panama"
+"""
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+"""
 
 
 # 첫 번째 풀이
-def valid_is_palind_room(s: str) -> bool:
+def valid_is_palind_room1(s: str) -> bool:
     strs = []
     for char in s:
         if char.isalnum():
@@ -16,4 +34,17 @@ def valid_is_palind_room(s: str) -> bool:
     return True
 
 
-print(valid_is_palind_room(s))
+# 두 번째 풀이
+def valid_is_palind_room2(s: str) -> bool:
+    from collections import deque
+
+    strs = deque()
+    for char in s:
+        if char.isalnum():  # 알파벳이나 숫자라면
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+
+    return True
