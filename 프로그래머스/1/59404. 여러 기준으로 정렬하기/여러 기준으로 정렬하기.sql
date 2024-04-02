f2d18825d8d2fbb9896ce81c6e142f9296d18d33
nick <alskadmlcraz1@gmail.com>
2024-04-02 19:46:54 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -0,0 +1,11 @@
+# select animal_id, name, datetime from animal_ins
+# order by name, datetime desc
+
+# # -- 코드를 입력하세요
+# # select animal_id, name, datetime
+# # from animal_ins
+# # order by name, datetime desc
+
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC
