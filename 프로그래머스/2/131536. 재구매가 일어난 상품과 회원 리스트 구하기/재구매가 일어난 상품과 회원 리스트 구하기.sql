e3fd705e17e4f9c004756184d524777118d312ae
nick <alskadmlcraz1@gmail.com>
2024-04-02 18:25:27 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,12 @@
+# -- 코드를 입력하세요
+# SELECT USER_ID, PRODUCT_ID, COUNT(*)
+# FROM ONLINE_SALE 
+# GROUP BY USER_ID, PRODUCT_ID
+# HAVING COUNT(*) > 1
+# ORDER BY USER_ID, PRODUCT_ID DESC
+
+select user_id, PRODUCT_ID
+from online_sale
+group by 1, 2
+having count(*) > 1
+order by user_id asc, PRODUCT_ID desc
